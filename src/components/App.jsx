3402e9d1d2db9d20550057796f601a43f315b5f7
sava <savamedvedev@gmail.com>
2022-06-30 23:30:02 +0800

    four-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
+import React, { useEffect } from "react";
+
 import Navbar from "./navbar/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./authorization/Registration";
+import Login from "./authorization/Login";
 
 function App() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  console.log(isAuth);
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
         <div className="wrap">
-          <Routes>
-            <Route path="/">
-              <Route path="registration" element={<Registration />} />
-            </Route>
-          </Routes>
+          {!isAuth && (
+            <Routes>
+              <Route path="/">
+                <Route path="registration" element={<Registration />} />
+                <Route path="login" element={<Login />} />
+              </Route>
+            </Routes>
+          )}
         </div>
       </div>
     </BrowserRouter>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import userReducer from "./fileReducer";
+import userReducer from "./userReducer";
 import fileReducer from "./userReducer";
 const rootStore = combineReducers({
   user: userReducer,
-  files: fileReducer,
 });
 
 export const store = createStore(
