98444c24a6955423a654b6b16ba98164cab07985
sava <savamedvedev@gmail.com>
2022-07-01 21:24:39 +0800

    EIGHT-commit

--- src/components/disk/Disk.jsx
+++ src/components/disk/Disk.jsx
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getFiles } from "../../actions/file";
+import { createDir, getFiles } from "../../actions/file";
+import { setPopupDisplay, setCurrentDir } from "../../reducers/fileReducer";
 import "./disk.css";
 import FileList from "./fileList/FileList";
+import Popup from "./Popup";
 
 const Disk = () => {
   const dispatch = useDispatch();
   const currentDir = useSelector((state) => state.files.currentDir);
+  const dirStack = useSelector((state) => state.files.dirStack);
 
   useEffect(() => {
     dispatch(getFiles(currentDir));
   }, [currentDir]);
 
+  function showPopupHandler() {
+    dispatch(setPopupDisplay("flex"));
+  }
+  function backClickHendler() {
+    const backDirId = dirStack.pop();
+    dispatch(setCurrentDir(backDirId));
+  }
+
   return (
     <div className="disk">
       <div className="disk__btns">
-        <button className="disk__back">Назад</button>
-        <button className="disk__create">Создать папку</button>
+        <button className="disk__back" onClick={() => backClickHendler()}>
+          Назад
+        </button>
+        <button className="disk__create" onClick={() => showPopupHandler()}>
+          Создать папку
+        </button>
       </div>
       <FileList />
+      <Popup />
     </div>
   );
 };
